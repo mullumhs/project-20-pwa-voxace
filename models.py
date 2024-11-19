17beb8a3f94338867c96b75a2a128334f979633a
David Steedman <dave@voxace.com>
2024-11-20 10:41:08 +1100

    Setup database model

--- models.py
+++ models.py
@@ -4,3 +4,15 @@ db = SQLAlchemy()
 
 # Define your database model here
 # Example: class Item(db.Model):
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    director = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
