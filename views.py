160bdd6188c60b9e5ea35a89f18ad811103e85e5
David Steedman <dave@voxace.com>
2024-11-29 08:46:35 +1100

    Working add and get

--- views.py
+++ views.py
@@ -1,36 +1,53 @@
 from flask import render_template, request, redirect, url_for, flash
-from models import db # Also import your database model here
-
-# Define your routes inside the 'init_routes' function
-# Feel free to rename the routes and functions as you see fit
-# You may need to use multiple methods such as POST and GET for each route
-# You can use render_template or redirect as appropriate
-# You can also use flash for displaying status messages
+from models import db, Movie
 
 def init_routes(app):
 
     @app.route('/', methods=['GET'])
-    def get_items():
-        # This route should retrieve all items from the database and display them on the page.
-        return render_template('index.html', message='Displaying all items')
-
-
+    def home():
+        movies = Movie.query.all()
+        return render_template('index.html', movies=movies)
 
     @app.route('/add', methods=['POST'])
-    def create_item():
-        # This route should handle adding a new item to the database.
-        return render_template('index.html', message='Item added successfully')
+    def add():
+        try:
+            # Get form data
+            title = request.form.get('title')
+            year = request.form.get('year')
+            director = request.form.get('director')
+            image_url = request.form.get('image_url')
+            genre = request.form.get('genre')
+            rating = request.form.get('rating')
 
+            # Create new movie instance
+            new_movie = Movie(
+                title=title,
+                year=int(year),
+                director=director,
+                image_url=image_url,
+                genre=genre,
+                rating=float(rating)
+            )
 
+            # Add to database
+            db.session.add(new_movie)
+            db.session.commit()
+
+            # Flash success message
+            flash('Movie added successfully!', 'success')
+        except Exception as e:
+            # If there's an error, rollback the session and flash error message
+            db.session.rollback()
+            flash(f'Error adding movie: {str(e)}', 'error')
+
+        return redirect(url_for('home'))
 
     @app.route('/update', methods=['POST'])
-    def update_item():
+    def update():
         # This route should handle updating an existing item identified by the given ID.
         return render_template('index.html', message=f'Item updated successfully')
 
-
-
     @app.route('/delete', methods=['POST'])
-    def delete_item():
+    def delete():
         # This route should handle deleting an existing item identified by the given ID.
         return render_template('index.html', message=f'Item deleted successfully')
