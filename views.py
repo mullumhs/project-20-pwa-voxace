17045ff339054cff8177451b4e07bf8ae0d23ca8
David Steedman <dave@voxace.com>
2024-12-04 12:34:23 +1100

    Working edit system

--- views.py
+++ views.py
@@ -7,6 +7,42 @@ def init_routes(app):
     def home():
         movies = Movie.query.all()
         return render_template('index.html', movies=movies)
+    
+    @app.route('/details', methods=['GET'])
+    def details():
+        id = request.args.get('id')
+        movie = Movie.query.get(id)
+        return render_template('details.html', movie=movie)
+
+    @app.route('/edit', methods=['GET','POST'])
+    def edit():
+        if request.method == 'POST':
+            try:
+                # Update movie with form data
+                id = request.form.get('id')
+                movie = Movie.query.get(id)
+                movie.title = request.form.get('title')
+                movie.year = int(request.form.get('year'))
+                movie.director = request.form.get('director')
+                movie.image_url = request.form.get('image_url')
+                movie.genre = request.form.get('genre')
+                movie.rating = float(request.form.get('rating'))
+
+                # Commit changes to database
+                db.session.commit()
+
+                # Flash success message
+                flash('Movie updated successfully!', 'success')
+                return redirect(url_for('details', id=id))
+            except Exception as e:
+                # If there's an error, rollback the session and flash error message
+                db.session.rollback()
+                flash(f'Error updating movie: {str(e)}', 'error')
+                return redirect(url_for('edit', id=id))
+        else:
+            id = request.args.get('id')
+            movie = Movie.query.get_or_404(id)
+            return render_template('edit.html', id=id, movie=movie)
 
     @app.route('/add', methods=['POST'])
     def add():
@@ -42,12 +78,14 @@ def init_routes(app):
 
         return redirect(url_for('home'))
 
-    @app.route('/update', methods=['POST'])
-    def update():
-        # This route should handle updating an existing item identified by the given ID.
-        return render_template('index.html', message=f'Item updated successfully')
-
     @app.route('/delete', methods=['POST'])
     def delete():
-        # This route should handle deleting an existing item identified by the given ID.
-        return render_template('index.html', message=f'Item deleted successfully')
+        movie = Movie.query.get_or_404(id)
+        try:
+            db.session.delete(movie)
+            db.session.commit()
+            flash('Movie deleted successfully!', 'success')
+        except Exception as e:
+            db.session.rollback()
+            flash(f'Error deleting movie: {str(e)}', 'error')
+        return redirect(url_for('home'))
